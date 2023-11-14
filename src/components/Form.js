6f0bf125634e2841311dcfa8797e03f3de67da03
Spy <Spyngunga@gmail.com>
2023-11-14 11:33:01 +0300

    Completed Assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Form(props) {
+function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
 
@@ -12,12 +12,42 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  const [errors, setErrors] = useState([]);
+  function handleSubmit(event) {
+    event.preventDefault();
+    // first name is required
+    if (firstName.length > 0) {
+      const formData = { 
+        firstName: firstName,
+         lastName: lastName 
+        };
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  }
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
-    <form>
+    <div>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    <h3>Submissions</h3>
+    {listofSubmissions}
+    </div>
   );
 }
 
